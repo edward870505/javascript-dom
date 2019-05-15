77dbe658fd293f468874cd7383571891b14c9820
edward@HOME <edward.huang@foxmail.com>
2019-05-15 23:31:47 +0800

    2019/5/15

--- 1.js
+++ 1.js
@@ -1,3 +1,6 @@
-var lis = document.getElementsByTagName('li');
-
-console.log(lis);
+var shopping = document.getElementById('purchases');
+//var items = shopping.getElementsByTagName('*');
+var items = document.getElementsByClassName('sale');
+for(var i=0,len=items.length;i<len;i++){
+    console.log(items[i]);
+}
